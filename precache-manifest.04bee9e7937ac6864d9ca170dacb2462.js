aa7ffd841af834a0b5f7b5cbb564698d9a8518eb
Buckshot-77 <Buckshot-77@github.com>
2019-12-15 20:23:59 -0300

    Updates

---
+++ precache-manifest.04bee9e7937ac6864d9ca170dacb2462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d5bc88ccea4c110063eb07f223b8fa",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "756cbbaebf846d90c382",
+    "url": "/Robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "/Robofriends/static/js/2.38e71f33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofriends/static/js/2.38e71f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "756cbbaebf846d90c382",
+    "url": "/Robofriends/static/js/main.db9a9d15.chunk.js"
+  },
+  {
+    "revision": "b0182db4def6c8c09d63",
+    "url": "/Robofriends/static/js/runtime-main.01aa462e.js"
+  }
+]);
